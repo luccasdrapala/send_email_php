c2f1cd48621c5a22d25016d5baffb6d2dcf57308
luccasdrapala <luccasdrapala@gmail.com>
2022-05-30 21:27:23 -0300

    configurando smtp

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 			<div class="py-3 text-center">
 				<img class="d-block mx-auto mb-2" src="logo.png" alt="" width="72" height="72">
 				<h2>Send Mail</h2>
-				<p class="lead">Seu app de envio de e-mails particular!</p>
+				<p class="lead">Meu app de envio de e-mails particular!</p>
 			</div>
 
       		<div class="row">
